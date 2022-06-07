be7426c170d10fcb40fd157198547f98baf9502d
Spencer Wood <spencer.wood@me.com>
2022-06-07 00:05:47 -0500

    added new route features and setup css for new look/feel of site

--- src/App.js
+++ src/App.js
@@ -1,46 +1,22 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
 import "./App.css";
-
-import CountDownTimer from "./CountDownTimer";
-import calculateDaysUntilBirthday from "./calculateDays";
+import Birthday from "./Birthday";
+import ReactDOM from "react-dom";
+import { Routes, Route } from "react-router-dom";
+import RouterBirthday from "./RouterBirthday";
 
 function App() {
-  const calculateDaysUntilBirthday = () => {
-    let today = new Date();
-    let year = today.getFullYear();
-    let birthday = new Date(year, 5, 30);
-    const difference = birthday - today;
-
-    let timeLeft = {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
-    };
-
-    return timeLeft.days;
-  };
-
-  const hoursMinSecs = {
-    hours: 600,
-    minutes: 13,
-    seconds: 30,
-  };
-
   return (
-    <Container fluid>
-      <Row className="justify-content-md-center">
-        <Col>
-          <h2>
-            Mady, it is
-            <h1>{calculateDaysUntilBirthday()}</h1>
-            {calculateDaysUntilBirthday() > 1 ? "days" : "day"} until your
-            birthday!
-          </h2>
-          {/* <CountDownTimer hoursMinSecs={hoursMinSecs} /> */}
-        </Col>
-      </Row>
-    </Container>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Birthday />} />
+        <Route
+          path="/birthday/:name?/:day?/:month?"
+          element={<RouterBirthday />}
+        />
+      </Routes>
+    </div>
   );
 }
+
 export default App;
---
+++ src/Generate.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Generate = () => {
+    const [name, setName] = useState('');
+    const[day, setDay] = useState('');
+    const [month, setMonth] = useState('');
+    const [link, setLink] = useState('');
+    const generateLink = ()
+}
